e53a15fb30769760c1734f7b9fd4f00348082ed1
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2026-01-01 10:00:07 +0800

    提示 请设置智谱AI API密钥

--- util/client/recv_result.py
+++ util/client/recv_result.py
@@ -46,10 +46,7 @@ async def recv_result():
 
             # AI优化语言表达
             ai_optimized_done = False
-            if (
-                Config.zhipuai_enable_ai_optimize_language_expression
-                and Config.zhipuai_api_key != ""
-            ):
+            if Config.zhipuai_enable_ai_optimize_language_expression:
                 ai_optimized_text, ai_delay = ai_optimize_language_expression(text)
                 ai_optimized_done = True
                 text = ai_optimized_text
